4a52a51a1dfcf3c9a2ce2f4657c078a0ce9d34af
gogy <1901862358@qq.com>
2024-03-14 20:58:52 +0800

    add all files

---
+++ antOptimization.py
@@ -0,0 +1,101 @@
+import numpy as np
+import random
+from numpy.random import choice as np_choice
+
+
+class AntColony():
+    def __init__(self, distances, n_ants, n_best, n_iterations, decay, alpha=1, beta=1):
+        """
+        distances : 2D array
+            Square matrix of distances. Diagonal is assumed to be np.inf.
+        n_ants : int
+            Number of ants running per iteration
+        n_best : int
+            Number of best ants who deposit pheromone
+        n_iteration : int
+            Number of iterations
+        decay : float
+            Rate it which pheromone decays. The pheromone value is multiplied by decay,
+            so 0.95 will lead to decay, 0.5 to much faster decay.
+        alpha : int or float
+            Exponenet on pheromone, higher alpha gives pheromone more weight. Default=1
+        beta : int or float
+            Exponent on distance, higher beta give distance more weight. Default=1
+        """
+        self.distances  = distances
+        self.pheromone = np.ones(self.distances.shape) / len(distances)
+        self.all_inds = range(len(distances))
+        self.n_ants = n_ants
+        self.n_best = n_best
+        self.n_iterations = n_iterations
+        self.decay = decay
+        self.alpha = alpha
+        self.beta = beta
+
+    def run(self):
+        shortest_path = None
+        all_time_shortest_path = ("placeholder", np.inf)
+        iteration_shortest_paths = []  # List to store each iteration's shortest path
+        for i in range(self.n_iterations):
+            all_paths = self.gen_all_paths()
+            self.spread_pheronome(all_paths, self.n_best, shortest_path=shortest_path)
+            shortest_path = min(all_paths, key=lambda x: x[1])
+            iteration_shortest_paths.append(shortest_path[1])  # Append current iteration's shortest distance
+            if shortest_path[1] < all_time_shortest_path[1]:
+                all_time_shortest_path = shortest_path            
+            self.pheromone = self.pheromone * self.decay            
+        return all_time_shortest_path, iteration_shortest_paths  # Return the all-time shortest path and list of iteration shortest paths
+
+
+    def spread_pheronome(self, all_paths, n_best, shortest_path):
+        sorted_paths = sorted(all_paths, key=lambda x: x[1])
+        for path, dist in sorted_paths[:n_best]:
+            for move in path:
+                self.pheromone[move] += 1.0 / self.distances[move]
+
+
+    def gen_path_dist(self, path):
+        """
+        calculate the distance of the path
+        """
+        total_dist = 0
+        for ele in path:
+            total_dist += self.distances[ele]
+        return total_dist
+
+    def gen_all_paths(self):
+        all_paths = []
+        for i in range(self.n_ants):
+            path = self.gen_path(0)
+            all_paths.append((path, self.gen_path_dist(path)))
+        return all_paths
+
+
+    def gen_path(self, start):
+        """
+        create a list of moves(in tuple) with length of the number of cities - 1
+        """
+        path = []
+        visited = set()
+        visited.add(start)
+        prev = start
+        for i in range(len(self.distances) - 1):
+            move = self.pick_move(self.pheromone[prev], self.distances[prev], visited)
+            path.append((prev, move))
+            prev = move
+            visited.add(move)
+        path.append((prev, start)) # going back to where we started    
+        return path
+
+    def pick_move(self, pheromone, dist, visited):
+        """
+        轮盘赌法
+        """
+        pheromone = np.copy(pheromone)
+        pheromone[list(visited)] = 0
+
+        row = pheromone ** self.alpha * (( 1.0 / dist) ** self.beta)    # possibility
+
+        norm_row = row / row.sum()
+        move = np_choice(self.all_inds, 1, p=norm_row)[0]   # p is the possibility of each move
+        return move
---
+++ run.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ant_optimization import AntColony
+
+def generate_random_distance_matrix(size):
+    # 生成一个随机的距离矩阵，矩阵大小为 size x size
+    np.random.seed(42)  # 设置随机种子以确保结果的可重复性
+    distances = np.random.randint(1, 100, size=(size, size))  # 生成随机距离，范围1到100
+    distances = (distances + distances.T) / 2  # 使矩阵对称，确保距离的一致性
+    np.fill_diagonal(distances, np.inf)  # 设置对角线为无穷大，表示自己到自己的距离
+    return distances
+
+# 生成80*80的随机距离矩阵
+size = 80
+distances = generate_random_distance_matrix(size)
+
+# 打印生成的矩阵
+print(distances)
+# 设定迭代次数
+iteration=50
+
+ant_colony = AntColony(distances, n_ants=100, n_best=10,
+                        n_iterations=iteration, decay=0.75, alpha=1, beta=2)
+shortest_path,dis = ant_colony.run()
+print ("shorted_path: {}".format(shortest_path))
+
+i = list(range(1,iteration+1))
+
+
+plt.figure()  # 设置图像大小
+plt.bar(i, dis)  # 画柱状图，X轴是迭代次数，Y轴是距离值
+
+# 设置图表标题和轴标签
+plt.title(f'Iteration ={iteration}')
+plt.xlabel('Iteration')
+plt.ylabel('Distance')
+# 显示图表
+plt.show()
+
+
